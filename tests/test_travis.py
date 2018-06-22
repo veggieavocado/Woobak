ccdc24f3cc834d2d9452df4f60c4a9c0ce26cae9
artlyhub <ppark9553@gmail.com>
2018-06-22 09:24:56 +0900

    깃허브 리포 시작과 Travis CI 테스트

---
+++ tests/test_travis.py
@@ -0,0 +1,17 @@
+'''
+This is a test script for pytest and Travis CI
+Refer to the code later as well/Do not delete file (Leave all tests untouched!!)
+
+Writer: 박시형
+Date: 6/22
+'''
+import pytest
+
+# test fixture
+@pytest.fixture
+def travis_worked_string():
+    return'travis worked!'
+
+def test_travis_worked(travis_worked_string):
+    # tests if travis ci works properly and that pytest fixtures also work well
+    assert travis_worked_string == 'travis worked!'
