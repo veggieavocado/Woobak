974692f538a30295efd1f943b25b6dd9ed7c867f
artlyhub <ppark9553@gmail.com>
2018-06-22 19:21:27 +0900

    자바스크립트 테스팅 환경 구축 첫단계

---
+++ js-src/testscript.js
@@ -0,0 +1,4 @@
+const testString = 'hello';
+const testString2 = 'there stranger!';
+
+console.log(testString + testString2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
 
   context: __dirname,
   devtool: 'source-map', // create source map so you can easily debug minified js files
 
   entry: {
-    test: ["babel-polyfill", __dirname + "/js-src/test.js"],
-    gateway: ["babel-polyfill", __dirname + "/js-src/gateway.js"],
+    test: ["babel-polyfill", __dirname + "/js-src/testscript.js"],
   },
 
   output: {
@@ -17,13 +18,33 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "eslint-loader",
+        }
+      },
+      {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-loader"}
+        ]
       }
     ]
   },
+
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 
 }
