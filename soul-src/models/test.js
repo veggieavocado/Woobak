00f49a0d5911b1e49a7a6a7b1086019f85728389
1 <ppark9553@gmail.com>
2018-07-01 23:25:07 +0900

    express 앱 수정: 소울 개발 환경 구축 완료

---
+++ soul-src/models/test.js
@@ -0,0 +1,12 @@
+// reference: https://velopert.com/594
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const testSchema = new Schema({
+  date: { type: Date, default: Date.now },
+  author: String,
+  content: String,
+});
+
+module.exports = mongoose.model('test', testSchema);
--- soul.js
+++ soul.js
@@ -3,10 +3,58 @@ const express = require('express');
 
 const app = express();
 
-app.listen(8080, async () => {
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Test = require('./soul-src/models/test');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// connect to MongoDB
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('Connected to MongoDB server');
+});
+
+// connect to localhost if not in Docker container
+const mongoURL = process.env.ENVIRONMENT === 'docker'
+  ? 'mongodb://mongo:27017/soul'
+  : 'mongodb://localhost:27017/soul';
+mongoose.connect(mongoURL);
+
+app.listen(8080, () => {
   console.log('Soul server started on port 8080');
 });
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   res.send('DONE');
+});
+
+// TEST API
+app.post('/api/test', (req, res) => {
+  // POST할 때 받은 데이터값을 몽고디비로 보내서 저장한다
+  // 저장에 성공하면 result가 1, 실패하면 0이다
+  const testInst = new Test();
+  testInst.author = req.body.author;
+  testInst.content = req.body.content;
+
+  testInst.save((error) => {
+    if (error) {
+      console.log(error);
+      res.json({ result: 0 });
+    }
+
+    res.json({ result: 1 });
+  });
+});
+
+// 트렐로 API
+app.get('/trello/board/:id', () => {
+  // retrieve board with id API
+});
+
+app.get('/trello/card/:id', () => {
+  // retrieve card with id API
 });
