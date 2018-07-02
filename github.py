84fce0efc2f23b2d13834340f10d942e5cbeeec2
wonseok <wonseok0403@naver.com>
2018-07-02 11:19:10 +0900

    wonseok test

---
+++ github.py
@@ -0,0 +1,6 @@
+GITHUB = {
+    'username': 'veggieavocado',
+    'password': 'dnqkrtjdrhd!1',
+    'repo': 'Woobak',
+    'key': 'a276e7981d789cde6b71dac78f227d6e6047db6f'
+}
--- tests/js/unit/trelloAPITest.js
+++ tests/js/unit/trelloAPITest.js
@@ -9,5 +9,8 @@ describe('Trello API 사용이 가능하다', async () => {
   it('트렐로에 로그인하는 것이 가능하다', async (done) => {
     chai.request(url)
   })
+  it('뭐가 또 가능하다. ', async(done) => {
+    
+  })
 
 })
