e7d5d2035327e7559ccfd2a57a442f4d563c1e16
1 <ppark9553@gmail.com>
2018-07-02 10:47:04 +0900

    원석 devops 브렌치 형성

---
+++ tests/js/unit/trelloAPITest.js
@@ -0,0 +1,13 @@
+// TEST STARTS HERE
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Trello API 사용이 가능하다', async () => {
+  it('트렐로에 로그인하는 것이 가능하다', async (done) => {
+    chai.request(url)
+  })
+
+})
