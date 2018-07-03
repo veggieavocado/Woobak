d993f901c67d2e1aaff4ec1e02f93130d43ffa4f
wonseok <wonseok0403@naver.com>
2018-07-03 12:26:23 +0900

    DB/API Module unit test done

--- tests/js/unit/trelloAPITest.js
+++ tests/js/unit/trelloAPITest.js
@@ -11,24 +11,10 @@ const expect = chai.expect;
 const BoardVeggieArvocadoID = 'akii79A7';
 chai.use(chaiHttp);
 
-const url = 'http://api.trello.com';
+const url = 'https://api.trello.com';
+const TestSpaceListVar = '5b39d61ac9ccf35c981d2f1a';
+const TestCardId = 'xbwwNz5G';
 
-describe('Request get wonseoks \'public\' board list', async () => {
-  it('Get board list test.', (done) => {
-    // Just change user37475319 --> your id
-    chai.request(`${url}/1/members/user37475319/boards`)
-      .get('/?fields=name')
-    // .set('key','b99f16d67481b93e65e19d84f64806ab')                                     # don't need in this function
-    // .set('token','1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53')   # don't need in this function
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        // console.log(res);                                                                # If you want to see raw data,
-        //                                                                                 # Delete '//'
-        done();
-      });
-  });
-});
 /*
     Fields of the board to be included in the response are,
     all, closed, dateLastActivity, dateLastView, desc, escData,
@@ -36,9 +22,33 @@ describe('Request get wonseoks \'public\' board list', async () => {
     name, pinned, powerUps, prefs, shortLink, shortUrl, starred,
     subscribed, url
 */
-describe('Request get board', async () => {
-  // in this test, I will jsut test board which name is 'VeggieAvocado'.
-  it('Get board information', (done) => {
+
+describe('GET API TEST', async() => {
+  
+  it('Get board list from member test. <server test>', (done) => {
+    // Just change user37475319 --> your id
+    chai.request(`${url}/1/members/user37475319/boards`)
+      .get('/?fields=name')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        // console.log(res);                                                                # If you want to see raw data,
+        done();
+      });
+  });
+
+  it('Get board\'s fields information test', (done) => {
+    chai.request(`${url}/1/boards/${BoardVeggieArvocadoID}`)
+      .get('?fields=id,name&lists=open&list_field=id,name')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        // console.log(res);
+        done();
+      });
+  });
+
+  it('Get board api', (done) => {
     chai.request(`${url}/1/boards/${BoardVeggieArvocadoID}`)
       .get('/?fields=idShort,name,idOrganization,labelNames')
       .set('key', 'b99f16d67481b93e65e19d84f64806ab')
@@ -50,7 +60,73 @@ describe('Request get board', async () => {
         done();
       });
   });
+  
+  it('Get list api', (done)=>{
+    chai.request(`${url}`)
+    .get('/1/boards/'+BoardVeggieArvocadoID)
+    .send({fields:'id, name', lists:'open', list_fields:'id, name, closed, pos'})
+    .end((err, res)=>{
+      // console.log(res);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    })
+  })
+
+
+  it('Get list api', (done)=>{
+    chai.request(`${url}`)
+    .get('/1/lists/' + TestSpaceListVar )
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',})
+    .end((err, res)=>{
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('Get list api<list-id>', (done)=>{
+    chai.request(`${url}`)
+    .get('/1/lists/' + TestSpaceListVar )
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53'})
+    .end((err, res)=>{
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('Get card api', (done)=>{
+    chai.request(`${url}`)
+    .get('/1/lists/'+TestSpaceListVar+'/cards')
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+           token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+           fields:'name'})
+    .end((err, res)=>{
+      // console.log(res);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('Get card api<card_id>', (done)=>{
+    chai.request(`${url}`)
+    .get('/1/cards/'+TestCardId)
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+    fields:'all'})
+    .end((err, res)=>{
+      // console.log(res);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
 });
+
 
 /*
 ** Get the Lists from Verggie Arvocado Board.
@@ -62,71 +138,77 @@ PEEPEE: https://api.trello.com/1/boards/akii79A7/lists
 위 링크도 작동하는 것으로 보임
 ********** */
 describe('Get all lists from VeggieArvocado', async () => {
-  it('It should be succeed!', (done) => {
-    chai.request(`${url}/1/boards/${BoardVeggieArvocadoID}`)
-      .get('?fields=id,name&lists=open&list_field=id,name')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        // console.log(res);
-        done();
-      });
-  });
 
   // list랑 card를 아예 타겟해서 정보 가져오거나 수정하는 것도 가능하더라고
   // https://developers.trello.com/reference#lists
   // https://developers.trello.com/reference#cards-1
-  it("also works with peepee's API URL", (done) => {
-    chai.request(`${url}/1/boards/${BoardVeggieArvocadoID}/lists`)
-      .get('/')
+});
+
+describe("POST api test", async() => {
+  const _key = 'b99f16d67481b93e65e19d84f64806ab';
+  const _token = '1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53';
+  const _testidBoard = '5b27804acfb5b8b8392dbc08';
+  it('List post api test', (done) =>{
+    chai.request(`${url}`)
+    .post('/1/lists')
+    // .set('key','b99f16d67481b93e65e19d84f64806ab')
+    // .set('token','1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53')
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+      name:'POST API TESTED', idBoard:`5b27804acfb5b8b8392dbc08`})
+    .end((err, res) => {
+      // console.log(res);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+
+  it('Card post api test', (done)=>{
+    chai.request(`${url}`)
+    .post('/1/cards')
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+      token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+      idList:TestSpaceListVar,
+      keepFromSource:'all',
+      name:'CARD POST API TEST'})
       .end((err, res) => {
+        // console.log(res);
         expect(err).to.be.null;
         expect(res).to.have.status(200);
         done();
-      });
+      })
   });
 });
 
-const TestSpaceListVar = '5b39d61ac9ccf35c981d2f1a';
-/* Test space list id is 5b39d61ac9ccf35c981d2f1a
-** You have to get your own list id.
-** Below test will check update well.
-
-** ....
-*/
-describe('Update list\'s name to "TESTTEST"', async () => {
-  // it('Update name will be succeed!', (done) => {
-  //   chai.request(`${url}/1/lists/`)
-  //     .put(TestSpaceListVar)
-  //     .query({ name: 'TESTTEST' })
-  //     .query({ key: 'b99f16d67481b93e65e19d84f64806ab' })
-  //     .query({ token: '1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53' })
-  //     .end((err, res) => {
-  //       expect(err).to.be.not.ok;
-  //       expect(res).to.have.status(200);
-  //       console.log(res);
-  //       done();
-  //     });
+describe("PUT API TEST", async()=>{
+  it('List put api test', (done) => {
+    chai.request(`${url}`)
+    .put('/1/lists/'+TestSpaceListVar)
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+    name:'PUT API TESTED',})
+    .end((err, res) =>{
+      // console.log(res);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    })
+  });
+  it('Card put api test', (done) =>{
+    chai.request(`${url}`)
+    .put('/1/cards/'+TestCardId)
+    .send({key:'b99f16d67481b93e65e19d84f64806ab',
+    token:'1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53',
+    name:'PUT API UPDATE TESTED',})
+    .end((err, res) =>{
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    })
+  })
+  // it('Card put api test', (done) => {
+  //   chai.request(`${url}`)
+  //   .put('/1/cards/'+)
   // });
-
-  it('Update name will work with key and token in URL', (done) => {
-    // url에 키랑 토큰을 넣어서 하면 해결이 되는 것 같다!! (ft. peepee)
-    // https://developers.trello.com/reference#introduction : API Key & Tokens 참조
-    const key = 'b99f16d67481b93e65e19d84f64806ab';
-    const token = '1212888ff2d1140637a2e9f0db08c011ca4ad13345fb9bf06ae2018447e9ee53';
-    chai.request(`${url}/1/lists/`)
-      .put(`${TestSpaceListVar}/?key=${key}&token=${token}`)
-      .query({ name: 'TESTTEST' })
-      .end((err, res) => {
-        expect(err).to.be.not.ok;
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-});
-
-
-// API testing here
-describe('Trello API wrappers', (done) => {
-  // test
 });
