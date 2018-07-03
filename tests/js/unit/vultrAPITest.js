9773d3818c1718a7c151e9739161989b997b6949
wonseok <wonseok0403@naver.com>
2018-07-04 06:06:29 +0900

    travis has problem

--- tests/js/unit/vultrAPITest.js
+++ tests/js/unit/vultrAPITest.js
@@ -13,52 +13,93 @@ var VultrAPI = require( 'vultr-api-wrapper' );
 // Create an instance with your Vultr API key
 var Vultr = new VultrAPI( { api_key: vultrAPI } );
 
-Vultr.server_list( function(error, statuscode, result){
-    console.log(error);
-    console.log(result);
-});
-describe("Show server list test", function(){
-    it('Show all servers..', function(){
+
+describe.only("Show server list test", function(){
+    this.timeout(5000);
+    it('Show all servers..',function(done){
         Vultr.server_list( function(error, statuscode, result){
-            console.log(error);
-            console.log(result);
-            expect(statuscode).to.have.status(200);
+            // console.log("I am here!");
+            // console.log(error);
+            // console.log(result);
+            expect(result).to.exist;
+            done();
         });
     });
 });
 
-describe("Show OS Lists", function(){
-    it('Show all server OS Lists ...', function(){
+describe.only("Show OS Lists", function(){
+    it('Show all server OS Lists ...', function(done){
         Vultr.os_list(function(error, status, result){
         //     console.log("I am here!");
         //     console.log(error);
         //     console.log(result);
         //     console.log(status);
-            expect(statuscode).to.have.status(200);
+        expect(result).to.exist;
+        done();
         });
     });
 });
 
-describe("Show DCID Lists", function(){
-    it('Show all DCID Lists ...', function(){
+describe.only("Show DCID Lists", function(){
+    it('Show all DCID Lists ...', function(done){
         Vultr.regions_list(function(error, status, result){
             // console.log("I am here!");
             // console.log(error);
             // console.log(result);
             // console.log(status);
-            expect(statuscode).to.have.status(200);
+            expect(result).to.exist;
+            done();
         });
     });
 });
 
 
-describe("Create server test", function(){
-    it('check the details.... ', function(){
-        Vultr.server_create({DCID:40, VPSPLANID:1, OSID:253}, function(error, statuscode, result){
-            console.log("I am here!");
-            console.log(error);
-            console.log(result);
-            expect(statuscode).to.have.status(200);
+// this test Success!
+// but after u test, delete the server!
+
+
+
+
+
+describe.only("Show plan lists", function(){
+    it('check the plans.... ', function(done){
+        Vultr.plans_list( function(error, statuscode, result){
+            // console.log("I am here!");
+            // // console.log(error);
+            // // console.log(result);
+            // console.log('statuscode:' + statuscode)
+            expect(result).to.exist;
+            done();
         });
     });
 });
+
+
+// API TEST COMPLETE
+// DANGER. 
+
+// describe.only("Delete Server", function(){
+//     it('Delete the test server.... ',function(done){
+//         Vultr.server_destroy( {SUBID: 16925589}, function(error, statusCode, result){
+//             console.log("I am here!");
+//             console.log(error);
+//             console.log(result);
+//             console.log('statuscode:' + statusCode)
+//             expect(int(statusCode)).to.be.oneOf([200,201,202]);
+//             done();
+//         });
+//     });
+// });
+
+// describe.only("Create server test", function(){
+//     it('check the details.... ', function(){
+//         Vultr.server_create({DCID:40, VPSPLANID:201, OSID:253}, function(error, statuscode, result){
+//             // console.log("I am here!");
+//             // console.log(error);
+//             // console.log(result);
+//             console.log(statuscode);
+//             console.log("I am here");
+//             expect(statuscode).to.have.status(200);
+//         })
+//     });
+// });
