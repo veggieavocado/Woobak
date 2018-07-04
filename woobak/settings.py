0c218ca6662e65218e2aeb4df6a88b0c761aad49
1 <ppark9553@gmail.com>
2018-07-04 13:26:02 +0900

    configs로 이름 바꿈

--- woobak/settings.py
+++ woobak/settings.py
@@ -2,12 +2,8 @@ import os
 import raven
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 SECRET_KEY = '(1*))8!e15ouzj635vf&of1l#x)n%w!hiw2k@!^rnq8sv!zssx'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.1.1', 'veggieavocado.com', 'www.veggieavocado.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -17,15 +13,37 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'raven.contrib.django.raven_compat',
+
+    'autoserver',
 ]
 
-# add Sentry settings here
-RAVEN_CONFIG = {
-    'dsn': 'https://3bb8b37ca3684f2383fd0562541a60b6:540ce3c5c92c4d069671bb69d0dcaae5@sentry.io/1230682',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
-}
+try:
+    # TRAVIS 환경변수를 들고와서 testing 변수에 넣어주기
+    # 환경변수가 없어서 에러가 발생하면 testing을 'False'로 세팅
+    testing = os.environ['TRAVIS']
+    print(testing)
+except:
+    testing = 'False'
+
+if testing == 'False':
+    INSTALLED_APPS.append('configs') # Travis CI 테스팅 때는 config없이 돌아갈 수 있도록 설정
+
+    from config.base import CONFIG, THIS_SYSTEM
+    if CONFIG['common']['DEBUG'] == 'True':
+        DEBUG = True
+    else:
+        DEBUG = False
+    # add Sentry settings here
+    # Sentry도 마찬가지로 Travis CI에서는 필요없음
+    RAVEN_CONFIG = {
+        'dsn': CONFIG['common']['SENTRY_URL'],
+        # If you are using git, you can also automatically configure the
+        # release based on the git info.
+        'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    }
+
+    if THIS_SYSTEM == 'web':
+        ALLOWED_HOSTS.append(CONFIG['ip-address']['web'])
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
