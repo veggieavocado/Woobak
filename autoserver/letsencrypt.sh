4a1460033f36d6c6637c3f19d48a02c89ad730b0
1 <ppark9553@gmail.com>
2018-07-04 21:42:12 +0900

    오토서버 다시 시도

--- autoserver/letsencrypt.sh
+++ autoserver/letsencrypt.sh
@@ -7,5 +7,5 @@ sudo apt-get update
 sudo apt-get install python-certbot-nginx
 sudo nginx -t
 sudo systemctl reload nginx
-sudo certbot --nginx -d veggieavocado.com www.veggieavocado.com
+sudo certbot --nginx -d veggieavocado.com -d www.veggieavocado.com
 sudo certbot renew --dry-run # certbot auto-renewal
