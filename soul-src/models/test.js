1bf9aaabf2cf153f6727aef066c37fbffdb81a8f
wonseok <wonseok0403@naver.com>
2018-07-09 11:45:07 +0900

    mongoose test

--- soul-src/models/test.js
+++ soul-src/models/test.js
@@ -1,12 +1,28 @@
 // reference: https://velopert.com/594
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+var mongodb = 'mongodb://127.0.0.1/test';
+mongoose.connect(mongodb);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
 
-const testSchema = new Schema({
-  date: { type: Date, default: Date.now },
-  author: String,
-  content: String,
-});
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-module.exports = mongoose.model('test', testSchema);
+
+
+
+
+
+//  !-- PP Example -- //
+// const Schema = mongoose.Schema;
+
+// const testSchema = new Schema({
+//   date: { type: Date, default: Date.now },
+//   author: String,
+//   content: String,
+// });
+
+// module.exports = mongoose.model('test', testSchema);
