e2f76cc8e9275f61910a973a78f810e16604c699
1 <ppark9553@gmail.com>
2018-07-11 14:22:29 +0900

    관리자/데브옵스 페이지 추가 완료

--- soul-src/models/test.js
+++ soul-src/models/test.js
@@ -1,31 +1,3 @@
-// reference: https://velopert.com/594
-var mongoose = require('mongoose');
-
-var mongodb = 'mongodb://127.0.0.1/test';
-mongoose.connect(mongodb);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-var db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
-
-
-
-
-//  !-- PP Example -- //
-// const Schema = mongoose.Schema;
-
-// const testSchema = new Schema({
-//   date: { type: Date, default: Date.now },
-//   author: String,
-//   content: String,
-// });
-
-// module.exports = mongoose.model('test', testSchema);
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
