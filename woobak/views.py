6e5a019222d022b004ec19f0d554fd35442dabb1
1 <ppark9553@gmail.com>
2018-07-13 12:12:54 +0900

    베타 페이지 세개 등록

--- woobak/views.py
+++ woobak/views.py
@@ -5,3 +5,18 @@ from django.views import View
 class TestView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'test.html', {})
+
+
+class BetaHomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home.html', {})
+
+
+class BetaLoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html', {})
+
+
+class BetaRegisterView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register.html', {})
