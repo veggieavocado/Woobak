d8f8cc81a7e9a3431cc683cf3c21a5fdc48cbf2f
MH-Lee <lmhoon012@gmail.com>
2018-07-18 21:53:45 +0900

    async await 추가

--- js-src/templateView.js
+++ js-src/templateView.js
@@ -1,2 +1,12 @@
-const hello = 'hello';
-console.log(hello);
+const axios = require('axios');
+
+const templateURL = 'http://127.0.0.1:8000/api/text/';
+
+const getTemplateData = async () => {
+  const templateData = await axios.get(templateURL);
+  console.log(templateData);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTemplateData();
+});
